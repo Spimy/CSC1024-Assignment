57e8ae283f7a3736b0f4a497f8974a5296e46016
Spimy <officialspyme@gmail.com>
2023-11-13 20:30:54 +0800

    📦 NEW: Add example menu

---
+++ menus/example_menu.py
@@ -0,0 +1,22 @@
+from .base_menu import BaseMenu
+
+
+class ExampleMenu(BaseMenu):
+    header = \
+        '''
+         _____                           _        __  __                  
+        | ____|_  ____ _ _ __ ___  _ __ | | ___  |  \/  | ___ _ __  _   _ 
+        |  _| \ \/ / _` | '_ ` _ \| '_ \| |/ _ \ | |\/| |/ _ \ '_ \| | | |
+        | |___ >  < (_| | | | | | | |_) | |  __/ | |  | |  __/ | | | |_| |
+        |_____/_/\_\__,_|_| |_| |_| .__/|_|\___| |_|  |_|\___|_| |_|\__,_|
+                                  |_|                                     
+        '''
+
+    def __init__(self, root=None):
+        BaseMenu.__init__(self, title='Example Menu',
+                          header=self.header, root=root)
+
+    def selection(self):
+        print('test')
+        input('Hit enter to go back to main menu...')
+        self.root.display().selection()
