9ab96ae9dc26b5bba7834010649858ca3a9d0c8b
Justin-yww <justinyong2003@yahoo.com>
2023-11-16 09:10:23 +0800

    Add Menu: Added update_edit_menu.py

---
+++ menus/update_edit_menu.py
@@ -0,0 +1,32 @@
+from utils import BaseMenu
+
+
+class UpdateMenu(BaseMenu):
+    header = \
+        '''
+         _____                           _        __  __                  
+        | ____|_  ____ _ _ __ ___  _ __ | | ___  |  \/  | ___ _ __  _   _ 
+        |  _| \ \/ / _` | '_ ` _ \| '_ \| |/ _ \ | |\/| |/ _ \ '_ \| | | |
+        | |___ >  < (_| | | | | | | |_) | |  __/ | |  | |  __/ | | | |_| |
+        |_____/_/\_\__,_|_| |_| |_| .__/|_|\___| |_|  |_|\___|_| |_|\__,_|
+                                  |_|                                     
+        '''
+
+    def __init__(self, root):
+        super().__init__(title='Update & Edit Books', header=self.header, root=root)
+
+        # NOTE: This is done purely for intellisense to work
+        # If you do not need intellisense anymore, this line should be removed
+        self.root = root
+
+    def selection(self):
+        # Start coding from here
+        '''
+        for i in range(len(self.root.book_list)):
+            print(f'[{i + 1}] Book: {self.root.book_list[i].title}')
+        '''
+        
+
+        # To return to the main menu 
+        input('Hit enter to go back to main menu...')
+        self.root.display().selection()
