06d32b96ef432f5c86c1183c4603fcea2822d8dd
LaiYW11 <yungwei9870@gmail.com>
2023-11-16 15:21:25 +0800

    Add display menu

--- menus/display_menu.py
+++ menus/display_menu.py
@@ -23,5 +23,22 @@ class DisplayMenu(BaseMenu):
         for i in range(len(self.root.book_list)):
             print(f'[{i + 1}] Book: {self.root.book_list[i].title}')
 
+
+class BookDisplay:
+    def __init__(self):
+        self.book_file = 'books_23020043.txt'
+
+    def display_book(self):
+        with open(self.book_file, 'r') as file:
+            lines = file.readlines()
+            for i, line in enumerate(lines, start = 1):
+                print(f"{i}. {line.strip()}")
+
         input('Hit enter to go back to main menu...')
         self.root.display().selection()
+
+if __name__ == "__main__":
+    display = BookDisplay()
+    display.display_book()
+
+
