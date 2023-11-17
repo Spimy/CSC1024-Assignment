905b67b66aef85beb3b342c659559b686bee3b1a
JBBru-helloworld <jb.brubusiness3@gmail.com>
2023-11-17 16:24:26 +0800

    Add ISBN Checker

--- utils/input_helper.py
+++ utils/input_helper.py
@@ -6,3 +6,12 @@ class InputHelper:
         an array with a length greater than 1  
         '''
         return len(string.split(',')) > 1
+    
+    def is_isbn(self, isbn):
+        if self._contains_comma(isbn):
+            return False
+
+        if len(isbn) != 10 or len(isbn) != 13:
+            return False
+        
+        
