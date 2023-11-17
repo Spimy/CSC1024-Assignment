eeedc80a76559d3c161832b6245cd07ebe37c4ee
Spimy <officialspyme@gmail.com>
2023-11-17 16:19:36 +0800

    📦 NEW: Add helper to check if string contains comma

--- utils/input_helper.py
+++ utils/input_helper.py
@@ -1,2 +1,8 @@
 class InputHelper:
-    pass
+    def _contains_comma(self, string):
+        '''
+        Check if string contains a comma
+        If a string contains a comma then splitting it with a comma should return
+        an array with a length greater than 1  
+        '''
+        return len(string.split(',')) > 1
