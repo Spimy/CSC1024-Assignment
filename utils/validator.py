101b033147e3f9014863a06cf1b858bb2eaf4d12
JBBru-helloworld <jb.brubusiness3@gmail.com>
2023-11-19 17:52:16 +0800

    Return true only at the end

--- utils/validator.py
+++ utils/validator.py
@@ -50,54 +50,50 @@ class Validator:
             
             # Last character of an ISBN 10 Numbers can be an X
             # X is considered to have the value of 10
-            if isbn[9].lower() == 'X':
+            if isbn[-1].lower() == 'X':
                 sum += 10
             else:
-                sum += int(isbn[9])
+                sum += int(isbn[-1])
 
             
             # Check for a remainder 
             # If there is a remainder, isbn is invalid
             # Return boolean
-            if sum % 11 == 0:
-               return {'valid': True,
-                       'message': ''
-                        }    
-            else:
+            if sum % 11 != 0:
                return {
-                    'valid': False, 
-                    'message' : "ISBN is invalid! Your 10 digit number is not an ISBN. Please try again!"
-                    } 
-            
-        
+                'valid': False, 
+                'message' : "ISBN is invalid! Your 10 digit number is not an ISBN. Please try again!"
+                } 
+               
         # Check if isbn is equal to 13
         # If true check validity of isbn and return boolean
-        if len(isbn) == 13:
+        else:
 
-            # Consider the first 12 digits
-            # Multiply each consecutive digit by 1
-            # Multiply each second consecutive digit by 3
-            result1 = isbn[0] * 1 + isbn[2] * 1 + isbn[4] * 1 + isbn[6] * 1 + isbn[8] * 1 + isbn[10] * 1
-            result2 = isbn[1] * 3 + isbn[3] * 3 + isbn[5] * 3 + isbn[7] * 3 + isbn[9] * 3 + isbn[11] * 3
+                # Consider the first 12 digits
+                # Multiply each consecutive digit by 1
+                # Multiply each second consecutive digit by 3
+                result1 = isbn[0] * 1 + isbn[2] * 1 + isbn[4] * 1 + isbn[6] * 1 + isbn[8] * 1 + isbn[10] * 1
+                result2 = isbn[1] * 3 + isbn[3] * 3 + isbn[5] * 3 + isbn[7] * 3 + isbn[9] * 3 + isbn[11] * 3
 
-            # Add both results of the sum of the multiplications
-            sum = result1 + result2
+                # Add both results of the sum of the multiplications
+                sum = result1 + result2
 
-            # Divide the sum to obtain remainder and substract 10
-            x = 10 - (sum % 10)
+                # Divide the sum to obtain remainder and substract 10
+                x = 10 - (sum % 10)
 
-            
-            # If x equals to the last digit of the isbn then its valid
-            # Return boolean
-            if x == isbn[12]:
-               return {'valid': True,
-                       'message': ''
-                        }    
-            else:
-                return {
-                    'valid': False, 
-                    'message' : "ISBN is invalid! Your 13 digit number is not an ISBN. Please try again!"
-                    } 
+                
+                # If x equals to the last digit of the isbn then its valid
+                # Return boolean
+                if x != isbn[12]:
+                    return {
+                        'valid': False, 
+                        'message' : "ISBN is invalid! Your 13 digit number is not an ISBN. Please try again!"
+                    }
+                
+        return {
+            'valid': True,
+            'message': ''
+        }
     
 
     def is_valid_date(self, date_string):
