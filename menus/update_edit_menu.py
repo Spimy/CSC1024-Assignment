a2375f682e9ab74e841d451fe08ccd4fb9f6e210
Justin-yww <justinyong2003@yahoo.com>
2023-11-23 10:21:25 +0800

    Add Menu: ISBN + Author and Title selector

--- menus/update_edit_menu.py
+++ menus/update_edit_menu.py
@@ -32,13 +32,86 @@ class UpdateMenu(BaseMenu):
         ** Use while loop to allow user to be able to change value again 
         '''
 
-        # Prompt user for the ISBN or Author + Title 
-        # Check the ISBN or Author + Title 
+        # Menu to display options 
+        while True: 
+            # To test if the input is valid
+            try: 
+                print("Please choose which information you would like to enter")
+                print("[1] ISBN \n[2] Author and title")
+                choice = int(input("Please choose either option 1 or option 2: "))
+            except: 
+                print("Invalid input! Please try again")
+                choice = None
 
+            if (choice == 1) or (choice == 2): 
+                break 
+            else: 
+                continue
+
+        # Prompt user for the ISBN or Author + Title 
+        # ISBN
+        if choice == 1: 
+            while True:
+                # User input isbn and check validity
+                while True:
+                    isbn = input(
+                        f"{self.error_flags['isbn']['message'] if not self.error_flags['isbn']['valid'] else ''}Enter the International Standard Book Number (ISBN): "
+                    )
+
+                    # User may input isbn with dashes or spaces
+                    # This should be removed before function is run
+                    isbn = isbn.replace("-", "").replace(" ", "").upper()
+
+                    # Function from validator.py should check validity of isbn
+                    self.error_flags['isbn'] = self.root.validator.is_isbn(isbn)
+
+                    # If valid then no need to ask for inout again
+                    if self.error_flags['isbn']['valid']:
+                        break
+        # Author & Title
+        elif choice == 2: 
+            while True: 
+                # User input Author & Title and check validity
+                # Author First Name 
+                while True:
+                    first_name = input(
+                        f"{'[First Name should not consist a comma(s) or be empty] ' if self.error_flags['first_name'] else ''}Enter the Author's First Name: "
+                    )
+
+                    # Guard Clause - if first_name is valid then break
+                    # If not valid - set error_flag to true
+                    if self.root.validator.is_valid_string(first_name):
+                        break
+
+                    self.error_flags['first_name'] = True
+                # Author Surname 
+                while True:
+                    surname = input(
+                        f"{'[Surname should not consist a comma(s) or be empty] ' if self.error_flags['surname'] else ''}Enter the Author's Surname: "
+                    )
+
+                    # Guard Clause - if surname is valid then break
+                    # If not valid - set error_flag to true
+                    if self.root.validator.is_valid_string(surname):
+                        break
+
+                    self.error_flags['surname'] = True
+                # Allow user to input Title name
+                while True:
+                    title = input(
+                        f"{'[Title should not consist a comma(s) or be empty] ' if self.error_flags['title'] else ''}Enter the Title of the Book: "
+                    )
+
+                    # Guard Clause - if title is valid then break
+                    # If not valid - set error_flag to true
+                    if self.root.validator.is_valid_string(title):
+                        break
+
+                    self.error_flags['title'] = True
+
+        # Check the ISBN or Author + Title
         # System for the user to edit and update information 
 
-        
-        
 
         # To return to the main menu 
         input('Hit enter to go back to main menu...')
