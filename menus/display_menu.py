16e0f559eb31cf0ca00dbc21ea889d192cb4d001
LaiYW11 <yungwei9870@gmail.com>
2023-11-25 23:43:58 +0800

    Added more functions

--- menus/display_menu.py
+++ menus/display_menu.py
@@ -19,104 +19,95 @@ __________               __     .____    ._____.
         # If you do not need intellisense anymore, this line should be removed
         self.root = root
 
+    def display_data(self, data, column_widths):
+        # Display each row of data using for loop
+        for row in data:
+            self.display_row(row, column_widths)
+
+    def display_row(self, row, column_widths):
+        # Create a formatted string for a single row by pairing each item in row within their own respective column
+        row_line = " | ".join(f"{item:<{width}}" for item, width in zip(row, column_widths))
+        print(row_line)
+
+    def tabulated_table(self, header_line, data, column_widths):
+        # Display the header line and table body
+        print(header_line)
+        print("-" * len(header_line))
+        
+        # Calls the function to display every data in a table
+        self.display_data(data, column_widths)
+
+        print("-" * len(header_line))
+
+    def searched_results(self, search_term, data, column_widths, header_line):
+        #empty list to store search functions
+        searched_books = []
+
+        if search_term:
+            # Search for the given term in ISBN, AUTHOR, or TITLE
+            for row in data:
+                isbn, author, title = row[:3]
+                if search_term in isbn.lower() or search_term in author.lower() or search_term in title.lower():
+                    searched_books.append(row)
+
+            if searched_books:
+                # Display search results if there are matches
+                print("\nSearch Results:")
+                print(header_line)
+                print("-" * len(header_line))
+                self.display_data(searched_books, column_widths)
+                print("-" * len(header_line))
+            else:
+                print("No books found with the given search term.")
+        else:
+            print("Please search for a book by ISBN, Author, or Title.")
+
     def selection(self):
         headers = ["ISBN", "Author", "Title", "Publisher", "Genre", "Year Published", "Date Purchased", "Status"]
-        
-        #Empty list to store all the important information(isbn, author, title, etc...)
-        data = []
+        # Load all new book data from function load_data()
+        data = self.load_data()
 
-        #To loop the entire book list until there is none
-        for i in range(len(self.root.book_list)):
+        # Asterisk to unpack the data
+        # Calculate maximum length of each item in their column
+        # zip headers and column width to pair 
+        # (^) to center each headers with their own column width
+        column_widths = [max(len(str(item)) for item in column) for column in zip(headers, *data)] #CALCULATING COLUMN WIDTH ONLY
+        header_line = " | ".join(f"{header:^{width}}" for header, width in zip(headers, column_widths)) #ONLY FOR HEADERS
 
-            #assigning them to their respective variable so it is easier to deal with
-            isbn = self.root.book_list[i].isbn
-            author = self.root.book_list[i].author
-            title = self.root.book_list[i].title
-            publisher = self.root.book_list[i].publisher
-            genre = self.root.book_list[i].genre
-            year_published = self.root.book_list[i].year_published
-            date_purchased = self.root.book_list[i].date_purchased
-            status = self.root.book_list[i].status
+        # Calls the function to tabulate the table
+        self.tabulated_table(header_line, data, column_widths)
 
-            #adding the individual variables above into the list
-            data.append([isbn, author, title, publisher, genre, year_published, date_purchased, status])
-        
+        # Prompts user to search or return to main menu
+        user_choice = input("Search Books[Y] or Return to Main Menu[N]: ").lower()
 
-        #Calculate all the column width and finding the maximum column length
-        #Zip to store the headers list and data list into a single list
-        #Asterisk(*) to unpack all the data
-        column_widths = [max(len(str(item)) for item in column) for column in zip(headers, *data)]
-
-        #(^) used to center the header within the column width
-        #Zip to store the spacings of each width respectively
-        header_line = " | ".join(f"{header:^{width}}" for header, width in zip(headers, column_widths))
-        print(header_line)
-
-        #print lines underneath so it looks cleaner
-        print("-" * len(header_line))
-
-        #to loop for how many rows there are in data
-        for row in data:
-
-            #to arrange all the "|" nicely within the list
-            #zip to pair and store each row with its own respective column
-            row_line = " | ".join(f"{item:<{width}}" for item, width in zip(row, column_widths))
-            print(row_line)
-
-        print("-" * len(header_line))
-
-        #prompts user if they want to search for books to return to main menu
-        user_choice = input("Search Books[Y] or Return to Main Menu[N]:  ").lower()
-
-        # if choices are incorrect, question will loop continuously
+        #handles if the input are not correct
         while user_choice not in ['y', 'n']:
             user_choice = input("Invalid Input, Search Books[Y] or Exit to Main Menu[N]: ").lower()
 
         if user_choice == 'y':
             while True:
-                search_term = input("Search book by ISBN, AUTHOR, or TITLE (Type 'EXIT' to return to main menu): ").lower()
-                searched_books = []
-
-                # if user enters exit, program will return to main menu
+                search_term = input("Search book by ISBN, AUTHOR, or TITLE (Type 'EXIT' to return to the main menu): ").lower()
                 if search_term == 'exit':
+                    # Return to the main menu if the user chooses to exit the search
                     self.root.display().selection()
+                    break
 
-                # check if the search term is not empty
-                if search_term:
-                    # for every row in data, check if search term matches ISBN, AUTHOR, or TITLE
-                    for row in data:
-                        isbn, author, title = row[:3]  # to allow search function for elements ISBN, AUTHOR, TITLE ONLY
+                #
+                self.searched_results(search_term, data, column_widths, header_line)
 
-                        # check if the search term is in ISBN, AUTHOR, or TITLE
-                        if search_term in isbn.lower() or search_term in author.lower() or search_term in title.lower():
-                            searched_books.append(row)
-
-                    # checks whether searched_books list is not empty
-                    if len(searched_books) > 0:
-                        print("\nSearch Results:")
-                        
-                        # print headers with '-' to make it look cleaner
-                        print(header_line)
-                        print("-" * len(header_line))
-
-                        # for the book found in the new list called searched_books
-                        for book in searched_books:
-                            # to arrange all the "|" nicely within the list
-                            # zip to pair the book with its own respective column_widths
-                            print(" | ".join(f"{item:<{width}}" for item, width in zip(book, column_widths)))
-
-                        # print lines underneath so it looks cleaner
-                        print("-" * len(header_line))
-
-                    else:
-                        print("No books found with the given search term.")
-                        # continues the program if no books are found
-                        continue
-
-                else:
-                    print("Please search for a book by ISBN, Author or Title.")
-
-        # if user enters 'n', program will return to the main menu
-        if user_choice == 'n':
+        elif user_choice == 'n':
+            # Return to the main menu if the user chooses not to search
             self.root.display().selection()
-            
+
+            return
+        
+    #Start of function
+    def load_data(self):
+        # Load data from the root's book_list and format it into a list
+        data = []
+        for i in range(len(self.root.book_list)): #adds every information in book_list into data
+            book = self.root.book_list[i]
+            data.append([book.isbn, book.author, book.title, book.publisher, book.genre, book.year_published, book.date_purchased, book.status])
+
+        #new data called will contain all book information
+        return data
