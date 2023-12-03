00f975166bc44319291f8cb2666e2391a02264a4
Spimy <officialspyme@gmail.com>
2023-12-03 16:20:13 +0800

    👌 IMPROVE: Add generic validator function to handle all user inputs

--- menus/add_menu.py
+++ menus/add_menu.py
@@ -11,27 +11,6 @@ class AddMenu(BaseMenu):
         /_/   \_\__,_|\__,_| |____/ \___/ \___/|_|\_\                                  
         '''
 
-    error_flags = {
-        'isbn': {
-            'valid': False,
-            'message': ''
-        },
-        'first_name': False,
-        'surname': False,
-        'title': False,
-        'publisher': False,
-        'genre': False,
-        'year_published': {
-            'valid': False,
-            'message': ''
-        },
-        'date_purchased': {
-            'valid': False,
-            'message': ''
-        },
-        'status': False
-    }
-
     def __init__(self, root):
         super().__init__(title='Add Books', header=self.header, root=root)
 
@@ -44,140 +23,50 @@ class AddMenu(BaseMenu):
         Allow user to input all book details and add it to the list
         Check for errors and allow user to re-input details if needed
         '''
-        
+
         while True:
-            # User input isbn and check validity
-            while True:
-                isbn = input(
-                    f"{self.error_flags['isbn']['message'] if not self.error_flags['isbn']['valid'] else ''}Enter the International Standard Book Number (ISBN): "
-                )
-
-                # User may input isbn with dashes or spaces
-                # This should be removed before function is run
-                isbn = isbn.replace("-", "").replace(" ", "").upper()
-
-                # Function from validator.py should check validity of isbn
-                self.error_flags['isbn'] = self.root.validator.is_isbn(isbn)
-
-                # If valid then no need to ask for inout again
-                if self.error_flags['isbn']['valid']:
-                    break
-
-            # Allow user to input name of Author
-            # For consistency prompt for first name first
-            while True:
-                first_name = input(
-                    f"{'[First Name should not consist a comma(s) or be empty] ' if self.error_flags['first_name'] else ''}Enter the Author's First Name: "
-                )
-
-                # Guard Clause - if first_name is valid then break
-                # If not valid - set error_flag to true
-                if self.root.validator.is_valid_string(first_name):
-                    break
-
-                self.error_flags['first_name'] = True
-
-            # For consistency prompt for surname second
-            while True:
-                surname = input(
-                    f"{'[Surname should not consist a comma(s) or be empty] ' if self.error_flags['surname'] else ''}Enter the Author's Surname: "
-                )
-
-                # Guard Clause - if surname is valid then break
-                # If not valid - set error_flag to true
-                if self.root.validator.is_valid_string(surname):
-                    break
-
-                self.error_flags['surname'] = True
-
-            # Allow user to input Title name
-            while True:
-                title = input(
-                    f"{'[Title should not consist a comma(s) or be empty] ' if self.error_flags['title'] else ''}Enter the Title of the Book: "
-                )
-
-                # Guard Clause - if title is valid then break
-                # If not valid - set error_flag to true
-                if self.root.validator.is_valid_string(title):
-                    break
-
-                self.error_flags['title'] = True
-
-            # Allow user to input Publisher
-            while True:
-                publisher = input(
-                    f"{'[Publisher should not consist a comma(s) or be empty] ' if self.error_flags['publisher'] else ''}Enter the Publisher: "
-                )
-
-                # Guard Clause - if publisher is valid then break
-                # If not valid - set error_flag to true
-                if self.root.validator.is_valid_string(publisher):
-                    break
-
-                self.error_flags['publisher'] = True
-
-            # Allow user to input Genre
-            while True:
-                genre = input(
-                    f"{'[Genre should not consist a comma(s) or be empty] ' if self.error_flags['genre'] else ''}Enter the Genre: "
-                )
-
-                # Guard Clause - if genre is valid then break
-                # If not valid - set error_flag to true
-                if self.root.validator.is_valid_string(genre):
-                    break
-
-                self.error_flags['genre'] = True
-
-            # Allow user to input Year Published
-            while True:
-                year_published = input(
-                    f"{self.error_flags['year_published']['message'] if not self.error_flags['year_published']['valid'] else ''}Enter the Year Published: "
-                )
-
-                # Function from validator.py should check validity of Year Published
-                # Should not contain comma(s)
-                # Function from validator.py should check validity of isbn
-                self.error_flags['year_published'] = self.root.validator.is_valid_year(
-                    year_published)
-
-                # If valid then no need to ask for inout again
-                if self.error_flags['year_published']['valid']:
-                    break
-
-            # Allow user to input Date Purchased
-            while True:
-                date_purchased = input(
-                    f"{self.error_flags['date_purchased']['message'] if not self.error_flags['date_purchased']['valid'] else ''}Enter the Date Purchased: "
-                )
-
-                # Function from validator.py should check validity of Date Purchased
-                # Should not contain comma(s)
-                # Function from validator.py should check validity of isbn
-                self.error_flags['date_purchased'] = self.root.validator.is_valid_date(
-                    date_purchased)
-
-                # If valid then no need to ask for inout again
-                if self.error_flags['date_purchased']['valid']:
-                    if int(date_purchased[6:len(date_purchased)]) < int(year_published):
-                        self.error_flags['date_purchased']['valid'] = False
-                        self.error_flags['date_purchased']['message'] = '[Year purchased cannot be less than the year published] '
-                        continue
-                    else:
-                        break
-
-            # Allow user to input whether they have read, are reading or still need to read the book
-            while True:
-                status = input(
-                    f"{'[Invalid Status] ' if self.error_flags['status'] else ''}Enter Book Status ('to-read', 'reading', 'read'): "
-                )
-
-                # Guard Clause - if status is allowed then break
-                # If not allowed - set error_flag to true
-                if self.root.validator.is_allowed_status(status):
-                    break
-
-                self.error_flags['status'] = True
+            isbn = self.validator_input(
+                display_string='Enter the International Standard Book Number (ISBN): ',
+                validator=self.root.validator.is_isbn
+            )
+            first_name = self.validator_input(
+                display_string="Enter the Author's First Name: ",
+                validator=self.root.validator.is_valid_string,
+                error_msg='First Name should not consist a comma(s) or be empty'
+            )
+            surname = self.validator_input(
+                display_string="Enter the Author's Surname: ",
+                validator=self.root.validator.is_valid_string,
+                error_msg='Surname should not consist a comma(s) or be empty'
+            )
+            title = self.validator_input(
+                display_string="Enter the Title of the Book: ",
+                validator=self.root.validator.is_valid_string,
+                error_msg='Title should not consist a comma(s) or be empty'
+            )
+            publisher = self.validator_input(
+                display_string="Enter the Publisher: ",
+                validator=self.root.validator.is_valid_string,
+                error_msg='Publisher should not consist a comma(s) or be empty'
+            )
+            genre = self.validator_input(
+                display_string="Enter the Genre: ",
+                validator=self.root.validator.is_valid_string,
+                error_msg='Genre should not consist a comma(s) or be empty'
+            )
+            year_published = self.validator_input(
+                display_string='Enter the Year Published: ',
+                validator=self.root.validator.is_valid_year
+            )
+            date_purchased = self.validator_input(
+                display_string='Enter the Date Purchased: ',
+                validator=self.root.validator.is_valid_date
+            )
+            status = self.validator_input(
+                display_string="Enter Book Status ('to-read', 'reading', 'read'):",
+                validator=self.root.validator.is_allowed_status,
+                error_msg='Invalid Status'
+            )
 
             # Capitalise first letters of some lettered attributes
             first_name = first_name[0].upper() + first_name[1:]
@@ -197,37 +86,42 @@ class AddMenu(BaseMenu):
             print("Book has successfully been added!")
             print()
 
-            # Reset flags so error codes are not initially run
-            self.error_flags = {
-                'isbn': {
-                    'valid': False,
-                    'message': ''
-                },
-                'first_name': False,
-                'surname': False,
-                'title': False,
-                'publisher': False,
-                'genre': False,
-                'year_published': {
-                    'valid': False,
-                    'message': ''
-                },
-                'date_purchased': {
-                    'valid': False,
-                    'message': ''
-                },
-                'status': False
-            }
-
             # Allow user to add another book or go back to main menu
-
             cont = ''
             while cont.upper() not in ('Y', 'N'):
                 cont = input("Do you wish to add another book (Y/N)?: ")
-                
+
             if cont.upper() == "Y":
                 self.display()
-                
+
             elif cont.upper() == "N":
                 self.root.display().selection()
                 break
+
+    def validator_input(self, display_string, validator, error_msg=''):
+        '''
+        Prompt user for input and validate it before returning a value
+        display_string - string to display when asking for user input
+        validator - callback function to use to validate user input
+        error_msg - optional parameter, the error message to display when validator returns a boolean
+        '''
+        display = display_string
+
+        while True:
+            user_input = input(display)
+            validator_result = validator(user_input)
+
+            # Handle validation for when the validator returns a dictionary
+            if type(validator_result) is dict:
+                if not validator_result['valid']:
+                    display = f'{validator_result["message"]} {display_string}'
+                    continue
+
+                return user_input
+
+            # Handle validation for when the validator returns a boolean
+            if not validator_result:
+                display = f'[{error_msg}] {display_string}'
+                continue
+
+            return user_input
