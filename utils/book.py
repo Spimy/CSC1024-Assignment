9612d3eba599ce03cc18d5097bfffeb46872d133
Spimy <officialspyme@gmail.com>
2023-12-05 23:08:02 +0800

    👌 IMPROVE: Override __str__ method instead of having to_string

--- utils/book.py
+++ utils/book.py
@@ -21,5 +21,5 @@ class Book:
         book = book_string.split(',')
         return Book(*book)
 
-    def to_string(self):
+    def __str__(self):
         return f'{self.isbn},{self.author},{self.title},{self.publisher},{self.genre},{self.year_published},{self.date_purchased},{self.status}'
